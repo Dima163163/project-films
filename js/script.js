c06ef915727eb3973bd64806cfbb785251ab23b0
Dima163163 <khlopovdima@gmail.com>
2023-07-11 19:15:39 +0400

    methods fix

--- js/script.js
+++ js/script.js
@@ -60,7 +60,6 @@ const personalMovieDB = {
 		console.log(this)
 	},
 	showMyDB: function (hidden) {
-		console.log('hidden',hidden)
 		if(!hidden){
 			console.log(this)
 		}
@@ -76,67 +75,4 @@ const personalMovieDB = {
 };
 
 personalMovieDB.logger()
-
-
-// let numberOfFilms;
-
-// function start () {
-// 	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-// 	while(numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms) || numberOfFilms === 0){
-// 		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-// 	}
-// }
-
-// start()
-
-// function rememberMyFilms () {
-// 	for (let i = 0; i < 2; i++){
-// 		const lastFilms = prompt('Один из просмотренных филмов?', '').trim();
-// 		const raitingFilms = prompt('На сколько оцените его?', '');
-
-// 		if(lastFilms != null && raitingFilms != null && lastFilms != '' && raitingFilms != '' && lastFilms.length < 50){
-// 			personalMovieDB.movies[lastFilms] = raitingFilms;
-// 		} else {
-// 			i--
-// 		}
-// 	};
-// }
-
-// rememberMyFilms();
-
-// function writeYourGeners() {
-// 	for(let i = 1 ; i <= 3; i++ ){
-// 		const genersOfFilms = prompt(`Ваш любимый жанр под номером ${i}`, '')
-// 		personalMovieDB.genres = [
-// 			...personalMovieDB.genres,
-// 			genersOfFilms
-// 		]
-// 	}
-// }
-
-// writeYourGeners()
-
-// function detectPersonalLevel() {
-// 	if (personalMovieDB.count < 10){
-// 		console.log("Просмотрено довольно мало фильмов");
-// 	} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-// 		console.log("Вы классический зритель");
-// 	} else if(personalMovieDB.count >= 30){
-// 		console.log("Вы классический зритель");
-// 	} else {
-// 		console.log("Произошла ошибка");
-// 	}
-// }
-
-// detectPersonalLevel()
-
-// function showMyDB(hidden) {
-	
-// 	if(!hidden){
-// 		console.log(personalMovieDB)
-// 	}
-// }
-// showMyDB(personalMovieDB.privat)
-
-
 
